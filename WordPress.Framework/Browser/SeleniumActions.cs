557ba0833c31c34ca1b3ca215c889be5cdd075cf
Administrator <Administrator@AUTFUNDAMENTALS>
2018-03-15 19:15:31 -0400

    Add project files.

---
+++ WordPress.Framework/Browser/SeleniumActions.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Threading;
+
+namespace WordPress.Framework.Browser
+{
+    public static class SeleniumActions
+    {
+        private static BrowserManager GetInstance
+        {
+            get
+            {
+                if (BrowserManager.Instance != null)
+                {
+                    return BrowserManager.Instance;
+                }
+
+                return null;
+            }
+        }
+
+        /// <summary>Gets the get web driver.</summary>
+        /// <value>The get web driver.</value>
+        public static IWebDriver GetWebDriver
+        {
+            get
+            {
+                if (GetInstance.Driver != null)
+                {
+                    return GetInstance.Driver;
+                }
+
+                return null;
+            }
+        }
+
+        public static WebDriverWait GetWaitDriver
+        {
+            get
+            {
+                if (GetInstance.Driver != null)
+                {
+                    return GetInstance.WaitDriver;
+                }
+
+                return null;
+            }
+        }
+
+        public static bool WaitForFrame(By iframe)
+        {
+            try
+            {
+                GetWaitDriver.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(iframe));
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Thread.Sleep(100);
+            }
+
+            return false;
+        }
+
+
+    }
+}
---
+++ WordPress.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPress.Framework.Browser;
+using WordPress.Framework.Pages;
+
+namespace WordPress.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            BrowserManager.Instance.Init();
+        }
+
+        [TestMethod]
+        public void User_Can_Login()
+        {
+            //Test Steps
+            LoginPage.GoTo();
+            LoginPage.LoginAs("Gonzalo")
+                     .WithPassword("Control123!")
+                     .Login();
+
+            //Validation
+            Assert.IsTrue(DashboardPage.IsAt);
+        }
+
+        [TestCleanup]
+        public void Clean()
+        {
+            BrowserManager.Instance.Close();
+        }
+    }
+}
